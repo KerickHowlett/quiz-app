4a222fae3c3c6f2b23afb634193ecf04bab079ae
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-04-29 00:42:19 -0400

    feat: navigate to login if unauthenticated

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/shared/bottom_nav.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/topics/topics.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('about'),
-          onPressed: () => Navigator.pushNamed(context, '/about'),
-        ),
-      ),
-      bottomNavigationBar: const BottomNavBar(),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('error'),
+          );
+        }
+        if (snapshot.hasData) {
+          return const TopicsScreen();
+        }
+        return const LoginScreen();
+      },
     );
   }
 }
